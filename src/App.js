5103a0d6e8c749ae56d60aaac0b26a5876208b04
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-10-26 00:32:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase'
+import List from './components/List';
 
 function App() {
+
+  const nome = "Maria"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <h1>Testando CSS</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Wilton"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome = {nome}/>
+      <Pessoa 
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foro="https://via.placeholder.com/150"
+      />
+      <List />
     </div>
+
   );
 }
 
