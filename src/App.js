28775c43f34b56de54244cdc7e777e112afb8485
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-10-27 22:11:13 -0300

    commit_6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
+
 //import Evento from './components/Evento'
 //import Form from './components/Form';
 
 function App() {
 
+  const meusItens = ["React", "Vue", "Angular"]
+
   return (
     <div className="App">    
-      <h1>Renderização Condicional</h1>     
-      <Condicional />
+      <h1>Renderização de Listas</h1>     
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
 
   )
